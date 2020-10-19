3ea37685efcdd0c9f4131f2a8bb9ef1b113e24c4
simon gunning <sigunning@hotmail.com>
2020-10-19 21:19:39 +0100

    Add new tables

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 
 mobileApp.tables.add('Course'); // Create a table for 'Course' with default settings
 mobileApp.tables.add('Player'); // Create a table for 'Player' with default settings
+mobileApp.tables.add('Society'); // Create a table for 'Society' with default settings
+mobileApp.tables.add('Event'); // Create a table for 'Event' with default settings
+mobileApp.tables.add('SocietyPlayer'); // Create a table for 'SocietyPlayer' with default settings
+
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
