3095ddcacbb9268e5b3053f29b7e1fded5d337a8
simon gunning <sigunning@hotmail.com>
2020-11-03 22:33:17 +0000

    Fix Round in server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ mobileApp.tables.add('Player'); // Create a table for 'Player' with default sett
 mobileApp.tables.add('Society'); // Create a table for 'Society' with default settings
 mobileApp.tables.add('Competition'); // Create a table for 'Competition' with default settings
 mobileApp.tables.add('SocietyPlayer'); // Create a table for 'SocietyPlayer' with default settings
+mobileApp.tables.add('Round'); // Create a table for 'Round' with default settings
 
 
 app.use(mobileApp);
